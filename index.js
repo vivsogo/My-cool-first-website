09e1b777abfc64b4a7cfac7453d16be562f557ab
Vivian Gomez <viviangomez0@gmail.com>
2021-10-01 23:26:34 -0400

    added draw fet

--- index.js
+++ index.js
@@ -3,19 +3,45 @@ console.log('h1',headerone)
 headerone.style.color= "yellow'";
 let test = document.getElementById("test");
 
-test.addEventListener("mouseleave", function( event ) {
-  event.target.style.color="Blue";
 
-  setTimeout(function() {
-    event.target.style.color = "";
-  }, 1000);
-}, false);
+let isDrawing = false;
+let x = 0;
+let y = 0;
 
-test.addEventListener("mouseout", function( event ) {
-  event.target.style.color = "orange";
+const myPics = document.getElementById('myPics');
+const context = myPics.getContext('2d');
 
-  setTimeout(function() {
-    event.target.style.color = "";
-  }, 500);
-}, false);
+myPics.addEventListener('mousedown', e => {
+  x = e.offsetX;
+  y = e.offsetY;
+  isDrawing = true;
+});
 
+myPics.addEventListener('mousemove', e => {
+  if (isDrawing === true) {
+    drawLine(context, x, y, e.offsetX, e.offsetY);
+    x = e.offsetX;
+    y = e.offsetY;
+  }
+});
+
+window.addEventListener('mouseup', e => {
+  if (isDrawing === true) {
+    drawLine(context, x, y, e.offsetX, e.offsetY);
+    x = 0;
+    y = 0;
+    isDrawing = false;
+  }
+});
+
+function drawLine(context, x1, y1, x2, y2) {
+  context.beginPath();
+  context.strokeStyle = 'black';
+  context.lineWidth = 1;
+  context.moveTo(x1, y1);
+  context.lineTo(x2, y2);
+  context.stroke();
+  context.closePath();
+}
+
+  
